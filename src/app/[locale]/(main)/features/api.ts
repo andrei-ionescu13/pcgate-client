f1a1eee1377b5559e11934010ba1917e8a8e7023
Andrei <andrei.ionescu3535@gmail.com>
2025-07-23 20:27:29 +0300

    remove code

--- src/app/[locale]/(main)/features/api.ts
+++
@@ -1,5 +0,0 @@
-import { Developer } from '@/types/developer';
-import { appFetch } from '@/utils/app-fetch';
-
-export const listFeatures = () =>
-  appFetch<Developer[]>({ url: '/features', withAuth: true });
